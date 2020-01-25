5c55909a7f8d9526a03c7b105127eaa96e953b94
Ray <584664105@qq.com>
2020-01-25 21:47:51 +0900

    simple rexx symbol parser

--- src/extension.ts
+++ src/extension.ts
@@ -5,27 +5,24 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
 	context.subscriptions.push(
 		vscode.languages.registerDocumentSymbolProvider(
-			{scheme: "file", language: "swmf-config"}, 
-			new SwmfConfigDocumentSymbolProvider()
+			{ scheme: "file", language: "rexx" },
+			new RexxConfigDocumentSymbolProvider()
 		)
 	);
 }
 
-class SwmfConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+class RexxConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
-	private format(cmd: string):string{
+	private format(cmd: string): string {
 		return cmd.substr(1).toLowerCase().replace(/^\w/, c => c.toUpperCase())
 	}
 
 	public provideDocumentSymbols(
 		document: vscode.TextDocument,
-		token: vscode.CancellationToken): Promise<vscode.DocumentSymbol[]> 
-		{
-		return new Promise((resolve, reject) => 
-		{
+		token: vscode.CancellationToken): Promise<vscode.DocumentSymbol[]> {
+		return new Promise((resolve, reject) => {
 			let symbols: vscode.DocumentSymbol[] = [];
 			let nodes = [symbols]
 			let inside_marker = false
@@ -41,76 +38,15 @@ class SwmfConfigDocumentSymbolProvider implements vscode.DocumentSymbolProvider 
 
 				let tokens = line.text.split(" ")
 
-				if (line.text.startsWith("#BEGIN_COMP")) {
-
-					let marker_symbol = new vscode.DocumentSymbol(
-						this.format(tokens[0]) + " " + tokens[1],
-						'Component',
-						symbolkind_marker,
-						line.range, line.range)
-
-
-					nodes[nodes.length-1].push(marker_symbol)
-					if (!inside_marker) {
-						nodes.push(marker_symbol.children)
-						inside_marker = true
-					}
-					// marker_symbol.children.push(_boot)
-				}
-				else if (line.text.startsWith("#END_COMP")) {
-					// TODO check if nodes has length 1 before popping.
-					if (inside_marker) {
-						nodes.pop()
-						inside_marker = false
-					}
-				}
-				else if (line.text.startsWith("#RUN") || line.text.startsWith("#END")) {
-					
-					let run_symbol = new vscode.DocumentSymbol(
-						this.format(tokens[0]),
-						'Session separator',
-						symbolkind_run,
-						line.range, line.range)
-
-					if (inside_run) {
-						nodes.pop()
-					}
-	
-					nodes[nodes.length-1].push(run_symbol)
-					nodes.push(run_symbol.children)
-					inside_run = true
-				}
-				else if (line.text.startsWith("#USERINPUTBEGIN")) {
-
-					let user_symbol = new vscode.DocumentSymbol(
-						this.format(tokens[0]),
-						'User module',
-						vscode.SymbolKind.Interface,
-						line.range, line.range)
-
-
-					nodes[nodes.length-1].push(user_symbol)
-					if (!inside_userinput) {
-						nodes.push(user_symbol.children)
-						inside_userinput = true
-					}
-					// marker_symbol.children.push(_boot)
-				}
-				else if (line.text.startsWith("#USERINPUTEND")) {
-					// TODO check if nodes has length 1 before popping.
-					if (inside_userinput) {
-						nodes.pop()
-						inside_userinput = false
-					}
-				}                
-				else if (line.text.startsWith("#")) {
+				if (line.text.match(/^\s*[@\d\w]+:/)) {
+					let colon_index = line.text.indexOf(":")
 					let cmd_symbol = new vscode.DocumentSymbol(
-						this.format(tokens[0]),
+						line.text.substring(1, colon_index-1),
 						'',
 						symbolkind_cmd,
 						line.range, line.range)
 
-					nodes[nodes.length-1].push(cmd_symbol)
+					nodes[nodes.length - 1].push(cmd_symbol)
 				}
 			}
 
